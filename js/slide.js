f690cf0e4d96bb01c67bcb0c1b5df78197922b55
Leticia Lopes <leticialopes236@gmail.com>
2023-07-08 15:13:59 -0300

    slide resize

--- js/slide.js
+++ js/slide.js
@@ -1,10 +1,13 @@
 // no mobile não é mousemove, é touchmove
 
+import debounce from './debounce.js'
+
 export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper)
     this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    this.activeClass = 'active'
   }
 
   transition(active) {
@@ -20,25 +23,25 @@ export default class Slide {
     let movetype;
     if (event.type === 'mousedown') {
       event.preventDefault();
-      this.dist.startX = event.clientX; // dentro do evento do mouse, em event tem o clientX (mesmo valor do pageX)  
-      movetype = 'mousemove'
+      this.dist.startX = event.clientX;
+      movetype = 'mousemove';
     } else {
-      this.dist.startX = event.changedTouches[0].clientX; // o primeiro é o 0 e dentro tem o clientX
-      movetype = 'touchmove'
+      this.dist.startX = event.changedTouches[0].clientX;
+      movetype = 'touchmove';
     }
     this.wrapper.addEventListener(movetype, this.onMove);
     this.transition(false);
   }
 
   updatePosition(clientX) {
-    this.dist.movement = (this.dist.startX - clientX) * 1.6; // * 1.6 pra acelerar o slide
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
     return this.dist.finalPosition - this.dist.movement;
   }
 
   onMove(event) {
-    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
     const finalPosition = this.updatePosition(pointerPosition);
-    this.moveSlide(finalPosition)
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
@@ -60,14 +63,14 @@ export default class Slide {
   }
 
   addSlideEvents() {
-    this.wrapper.addEventListener('mousedown', this.onStart); // mousedown é no momento do clique, antes de soltar
+    this.wrapper.addEventListener('mousedown', this.onStart);
     this.wrapper.addEventListener('touchstart', this.onStart);
-    this.wrapper.addEventListener('mouseup', this.onEnd)
-    this.wrapper.addEventListener('touchend', this.onEnd)
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+    this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
   slidePosition(slide) {
-    const margin = this.wrapper.offsetWidth - slide.offsetWidth / 2 // quando pegamos o wrapper total e diminuimos pela largura da foto, sobrará uma margem e aí só precisamos dividir por 2
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
     return -(slide.offsetLeft - margin);
   }
 
@@ -87,20 +90,18 @@ export default class Slide {
       next: index === last ? undefined : index + 1,
     }
   }
-
+  
   changeSlide(index) {
     const activeSlide = this.slideArray[index];
     this.moveSlide(activeSlide.position);
     this.slidesIndexNav(index);
     this.dist.finalPosition = activeSlide.position;
+    this.changeActiveClass();
   }
 
-  bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-    this.activePrevSlide = this.activePrevSlide.bind(this);
-    this.activeNextSlide = this.activeNextSlide.bind(this);
+  changeActiveClass() {
+    this.slideArray.forEach(item => item.element.classList.remove(this.activeClass));
+    this.slideArray[this.index.active].element.classList.add(this.activeClass);
   }
 
   activePrevSlide() {
@@ -111,11 +112,33 @@ export default class Slide {
     if (this.index.next !== undefined) this.changeSlide(this.index.next);
   }
 
+  onResize() {
+    setTimeout(() => {
+      this.slidesConfig();
+      this.changeSlide(this.index.active);
+    }, 1000)
+  }
+
+  addResizeEvent() {
+    window.addEventListener('resize', this.onResize)
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+    this.activePrevSlide = this.activePrevSlide.bind(this);
+    this.activeNextSlide = this.activeNextSlide.bind(this);
+
+    this.onResize = debounce(this.onResize.bind(this), 50);
+  }
+
   init() { // pra não dar erro devemos chamar o bind primeiro
     this.bindEvents();
     this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
+    this.addResizeEvent()
     return this;
   }
 }
